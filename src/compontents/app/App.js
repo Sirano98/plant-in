83876879aaf84c7a567512b5372d7a950dc2d607
Dmitry <protsiuk1998@gamil.com>
2022-01-01 14:37:23 +0200

    add: fonts

---
+++ src/compontents/app/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import { Header } from '../header/Header';
+import './App.css';
+
+function App() {
+    return (
+        <Fragment>
+            <Header />
+        </Fragment>
+    );
+}
+
+export default App;

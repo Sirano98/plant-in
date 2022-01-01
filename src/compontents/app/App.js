b84ca38b9cf7523c57b6944ed63c9ba549a54edb
Dmitry <protsiuk1998@gamil.com>
2022-01-02 00:45:25 +0200

    add: banner

--- src/compontents/app/App.js
+++ src/compontents/app/App.js
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 import { Banner } from '../banner/Banner';
 import { Header } from '../header/Header';
+import { Navigation } from '../navigation/Navigation';
 import './App.css';
 
 function App() {
     return (
         <Fragment>
             <Header />
+            <Navigation />
             <Banner />
         </Fragment>
     );
--- src/compontents/header/Header.js
+++ src/compontents/header/Header.js
@@ -14,7 +14,7 @@ export const Header = () => {
                         <a href="#" className="nav__link">Contact Us</a>
                     </nav>
                     <div className="header__block">
-                        <button className="header__search">
+                        <button className="search__button">
                             <svg viewBox="0 0 19 19" fill="none" className="search__icon">
                                 <path d="M13 13L18 18M15 8C15 11.866 11.866 15 8 15C4.13401 15 1 11.866 1 8C1 4.13401 4.13401 1 8 1C11.866 1 15 4.13401 15 8Z" stroke="#C5D1DB" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>

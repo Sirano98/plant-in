d93152aa63e4dffb91d93cee7fdf9260f404a27f
Dmitry <protsiuk1998@gamil.com>
2022-01-02 12:37:40 +0200

    refact: change input, aling user icon

--- src/compontents/banner/Banner.js
+++ src/compontents/banner/Banner.js
@@ -1,5 +1,7 @@
 import "./Banner.css";
-
+// обернуть инпунт в лейбл 
+// search placeholder
+// search псевдоэлемент
 export const Banner = () => {
     return (
         <div className="container">
@@ -8,18 +10,21 @@ export const Banner = () => {
                     <p className="banner__title">Stay always tuned with planting trends</p>
                     <p className="banner__text">Current temperature is: {24}°C</p>
                     <div className="input__wrapper">
-                        <label htmlFor="banner__input" className="input__label">
-                            <p>Search</p>
-                            <button className="search__button search__button-banner">
-                                <svg viewBox="0 0 19 19" fill="none" className="search__icon">
-                                    <path d="M13 13L18 18M15 8C15 11.866 11.866 15 8 15C4.13401 15 1 11.866 1 8C1 4.13401 4.13401 1 8 1C11.866 1 15 4.13401 15 8Z" stroke="#C5D1DB" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                </svg>
-                            </button>
-                        </label>
-                        <input className="banner__input" id="banner__input" />
+                        <input className="banner__input" id="banner__input" placeholder="Search" />
+                        <button className="search__button search__button-banner">
+                            <svg viewBox="0 0 19 19" fill="none" className="search__icon">
+                                <path d="M13 13L18 18M15 8C15 11.866 11.866 15 8 15C4.13401 15 1 11.866 1 8C1 4.13401 4.13401 1 8 1C11.866 1 15 4.13401 15 8Z" stroke="#C5D1DB" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                        </button>
                     </div>
                 </div>
             </section>
         </div>
     )
 }
+
+/* <button className="search__button search__button-banner">
+                                <svg viewBox="0 0 19 19" fill="none" className="search__icon">
+                                    <path d="M13 13L18 18M15 8C15 11.866 11.866 15 8 15C4.13401 15 1 11.866 1 8C1 4.13401 4.13401 1 8 1C11.866 1 15 4.13401 15 8Z" stroke="#C5D1DB" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                            </button> */

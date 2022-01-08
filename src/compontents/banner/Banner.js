9ac8e8abdf69f90017df8a0d5ddd8061aa6953d1
Dmitry <protsiuk1998@gamil.com>
2022-01-08 10:06:53 +0200

    add: getting weather

--- src/compontents/banner/Banner.js
+++ src/compontents/banner/Banner.js
@@ -1,11 +1,49 @@
+import { useEffect, useState } from "react";
 import "./Banner.css";
 
 export const Banner = () => {
+    const [temperature, setTemperature] = useState("-");
+
+    useEffect(() => {
+        getGeolocationData();
+    }, [])
+
+    const getGeolocationData = () => {
+        if (navigator) {
+            navigator.geolocation.getCurrentPosition(geoSuccess, geoError)
+        } else {
+            setTemperature("-");
+        }
+    }
+
+    const geoSuccess = (position) => {
+        getWeatherData(position.coords.longitude, position.coords.latitude);
+    }
+
+    const geoError = () => {
+        setTemperature("-");
+    }
+
+    const getWeatherData = (long, lat) => {
+        fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=9b8fbc52a5e4d0c87d96ed277b0822a8')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                const celsiusDegree = transfornTemperature(data.main.temp);
+                setTemperature(celsiusDegree);
+            })
+    }
+
+    const transfornTemperature = (degreesKelvin) => {
+        return Math.round(degreesKelvin - 273.15);
+    }
+
     return (
         <section className="banner">
             <div className="banner__content">
                 <p className="banner__title">Stay always tuned with planting trends</p>
-                <p className="banner__text">Current temperature is: {24}°C</p>
+                <p className="banner__text">Current temperature is: {temperature}°C</p>
                 <p className="banner__text banner__text-mobile">Tips &#38; Tricks selected specially for you!</p>
                 <div className="input__wrapper">
                     <input className="banner__input" id="banner__input" placeholder="Search" />
